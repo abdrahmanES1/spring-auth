a8ffa5c3979855f77c509926b54ca95e0b8c3a8c
abdrahmanES1 <e.abdrahman2002@gmail.com>
2024-08-09 10:40:55 +0100

    add docs

--- src/main/java/com/esab/springauth/config/AuthConfig.java
+++ src/main/java/com/esab/springauth/config/AuthConfig.java
@@ -1,10 +1,11 @@
 package com.esab.springauth.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.EnableCaching;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -23,6 +24,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.esab.springauth.config.auth.OurSecurityFilter;
 import com.esab.springauth.config.auth.SecurityLoggingFilter;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
@@ -36,12 +43,20 @@ public class AuthConfig {
   @Autowired
   private AuthenticationEntryPoint customAuthenticationEntryPoint;
 
+  private static final String[] SWAGGER_WHITELIST = {
+      "/swagger-ui/**",
+      "/v3/api-docs/**",
+      "/swagger-resources/**",
+      "/swagger-resources",
+  };
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(SWAGGER_WHITELIST).permitAll()
             .requestMatchers(HttpMethod.POST, "/api/v1/auth/*").permitAll()
             .requestMatchers("/api/v1/authors").permitAll()
             .requestMatchers(HttpMethod.GET, "/api/v1/books").hasRole("USER")
@@ -69,5 +84,27 @@ public class AuthConfig {
   static GrantedAuthorityDefaults grantedAuthorityDefaults() {
 
     return new GrantedAuthorityDefaults("ROLE_");
+  }
+
+  @Bean
+  GroupedOpenApi publicApi() {
+    return GroupedOpenApi
+        .builder()
+        .group("public-apis")
+        .pathsToMatch("/**")
+        .build();
+  }
+
+  @Bean
+  OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("API title").version("API vesion"))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(
+            new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
   }
 }
