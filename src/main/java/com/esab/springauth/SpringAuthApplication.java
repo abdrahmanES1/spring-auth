24ffd265335fdbb7ac52541df8744e1c0b031278
abdrahmanES1 <e.abdrahman2002@gmail.com>
2024-08-10 12:47:05 +0100

    feat: spring admin panel to check health of the application

--- src/main/java/com/esab/springauth/SpringAuthApplication.java
+++ src/main/java/com/esab/springauth/SpringAuthApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.context.annotation.PropertySource;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
 // @PropertySource("file:${user.dir}/.env")
+@EnableAdminServer
 public class SpringAuthApplication {
 
 	public static void main(String[] args) {
